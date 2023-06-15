ba3bfe6a1a97a6d9363f3d0b80bf2200cae90927
crazykim67 <crazykim67@gmail.com>
2023-06-15 09:55:04 +0900

    Import Assets

--- Assets/Scripts/ARController.cs
+++ Assets/Scripts/ARController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class ARController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject currentPlayerObj = null;
 
-    // Update is called once per frame
-    void Update()
+    public GameObject playerObj;
+    public ARRaycastManager raycastManager;
+
+    private bool isAct = false;
+
+    private void Update()
     {
-        
+        if (isAct)
+            return;
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+            raycastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+            if(hits.Count > 0 ) 
+            {
+                currentPlayerObj = Instantiate(playerObj, hits[0].pose.position, hits[0].pose.rotation);
+                isAct = true;
+            }
+        }
     }
 }
