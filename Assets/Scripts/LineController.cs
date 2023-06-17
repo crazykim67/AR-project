9c643d1d83c054b68ea9b2ada5176f934d5f19a0
crazykim67 <crazykim67@gmail.com>
2023-06-17 23:11:38 +0900

    LineController, PenTool 추가

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+    public LineRenderer lr;
+    public List<Transform> points;
+
+    private void Awake()
+    {
+        lr.positionCount = 0;
+
+        points = new List<Transform>();
+    }
+    public void ChangePoint(float _length, GameObject dotPrefab, Transform setParent)
+    {
+        //Debug.Log("before : " + points.Count);
+        Vector3 p0 = points[points.Count - 2].transform.position;
+        Vector3 p1 = points[points.Count - 1].transform.position;
+        float Distance = Vector3.Distance(p1, p0);
+        if (Distance <= _length) return;
+
+        // 프레임 단위로 그려지기 때문에 유저가 프레임 단위보다 빠른 속도로 선을 그으면
+        // 부자연스럽게 선이 그어지기 때문에 해당부분을 보완하는 부분
+        // 중간에 채워야할 포인트 수
+        int re = (int)(Distance / _length); 
+
+            GameObject dot;
+            
+        if(re > 1)
+                for (int i = 0; i < re; i++)
+                {
+                    dot = Instantiate(dotPrefab, new Vector3(0, 0.025f, 0), Quaternion.identity, setParent);
+                    dot.transform.position = Vector3.Lerp(p0, p1, (i+1) / ((float)(re + 1)));
+                    lr.positionCount++;
+                    points.Insert(points.Count - 1, dot.transform);
+                    //Debug.Log("Result : " + re);
+                    Debug.Log(p0);
+            }
+
+    }
+    public void AddPoint(Transform point)
+    {
+        lr.positionCount++;
+        points.Add(point);
+    }
+
+    private void LateUpdate()
+    {
+        if (points.Count >= 2)
+        {
+            for(int i = 0; i < points.Count; i++)
+            {
+                lr.SetPosition(i, points[i].position);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PenTool.cs
@@ -0,0 +1,162 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenTool : MonoBehaviour
+{
+    [Header("Dots")]
+    [SerializeField] private GameObject dotPrefab;
+    [SerializeField] private Transform dotParent;
+    [SerializeField] private float dotSize;
+    [SerializeField] private bool dotActive;
+
+    [Header("Lines")]
+    [SerializeField] private GameObject linePrefab;
+    [SerializeField] private Transform lineParent;
+    [SerializeField] private float lineSize;
+    [SerializeField] private bool lineActive;
+    [SerializeField] private float lineLength;
+
+    [Header("Speed")]
+    [SerializeField] private float Speed;
+
+    private LineController currentLine;
+    //public PlayerMove playerMove;
+    //public PlayerStatus playerStatus;
+
+    public Vector3 worldMousePosition;
+
+    public static bool isMove;
+    private bool isLine;
+    public GameObject playerObject;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        //if (playerStatus.isDead)
+        //    return;
+
+        #region GetMouseButtonDown
+
+        if (Input.GetMouseButtonDown(0) && !isMove) // MouseDown
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Player")
+                    isLine = true;
+
+            }
+        }
+
+        #endregion
+
+        #region GetMouseButton
+
+        if (Input.GetMouseButton(0) && !isMove) // GetMouseButton
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Plane")
+                {
+                    if (!isLine)
+                        return;
+
+                    worldMousePosition = hit.point;
+
+                    //worldMousePosition.y = 0.05f;
+
+                    if (currentLine == null)
+                    {
+                        currentLine = LineSet(linePrefab, lineActive, lineSize);
+                    }
+
+                    if (!CanAppend(worldMousePosition)) return; // 라인 길이 
+
+                    GameObject dotted;
+
+                    if (currentLine.lr.positionCount == 0)
+                    {
+                        dotted = InstantiateDotSet(dotPrefab, dotActive, dotSize, new Vector3(playerObject.transform.position.x, worldMousePosition.y, playerObject.transform.position.z));
+                        currentLine.AddPoint(dotted.transform);
+                    }
+                    else
+                    {
+                        dotted = InstantiateDotSet(dotPrefab, dotActive, dotSize, worldMousePosition);
+                        currentLine.AddPoint(dotted.transform);
+
+                        currentLine.ChangePoint(lineLength, dotPrefab, dotParent);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Runtime
+        if (currentLine != null) // Runtime 라인 조절
+        {
+            currentLine.lr.startWidth = lineSize;
+            currentLine.transform.gameObject.SetActive(lineActive);
+        }
+
+        if (currentLine != null) // Runtime 도트 조절
+        {
+            foreach(var dot in currentLine.points)
+            {
+
+                dot.transform.localScale = new Vector3(dotSize, dotSize, dotSize);
+                dot.transform.gameObject.SetActive(dotActive);
+            }
+        }
+        #endregion
+
+        #region GetMouseButtonUp
+
+        if (Input.GetMouseButtonUp(0) && !isMove && isLine) // Mouse Up
+        {
+            isLine = false;
+            isMove = true;
+        }
+
+        //if (isMove)
+        //    playerMove.MoveOn(currentLine, Speed, isMove);
+
+        #endregion
+    }
+
+    // 포인트 생성
+    public GameObject InstantiateDotSet(GameObject _obj, bool _act, float _size, Vector3 _pos) 
+    {
+
+        dotPrefab.transform.localScale = new Vector3(_size, _size, _size);
+
+        _obj.SetActive(_act);
+
+        GameObject dot = Instantiate(dotPrefab, _pos, Quaternion.identity, dotParent);
+
+        return dot;
+    }
+
+    // 라인렌더러 포지션 생성
+    private LineController LineSet(GameObject _obj, bool _act, float size)
+    {
+
+        _obj.GetComponent<LineController>().lr.startWidth = size;
+
+        _obj.SetActive(_act);
+
+        LineController Line = Instantiate(linePrefab, Vector3.zero, Quaternion.identity, lineParent).GetComponent<LineController>();
+
+        return Line;
+    }
+
+    // 라인렌더러 한 포지션 당 한계점
+    public bool CanAppend(Vector3 pos)
+    {
+        if (currentLine.lr.positionCount == 0) return true;
+
+        return Vector3.Distance(currentLine.lr.GetPosition(currentLine.lr.positionCount - 1), pos) >= lineLength;
+    }
+}
